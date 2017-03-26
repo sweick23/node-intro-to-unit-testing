ca77331a9fe6df316c488484f2ac3349805cd640
Shane Weickum <sweick23@outlook.com>
2017-03-26 09:46:12 -0700

    FizzBuzzer.js

--- test/test-adder.js
+++ test/test-adder.js
@@ -1,24 +1,24 @@
 const should = require('chai').should();
 
-const adder = require('../adder');
+const fizzBuzzer= require('../fizzBuzzer');
 
 
 // unit tests for our `adder` function
-describe('adder', function() {
+describe('fizzBuzzer', function() {
 
   // test the normal case
-  it('should add two numbers', function() {
+  it('should be divided by', function() {
     // range of normal inputs, including
     // notable cases like negative answers
     const normalCases = [
-      {a: 2, b: 3, expected: 5},
-      {a: 200, b: 2000, expected: 2200},
-      {a: 2, b: -5, expected: -3}
+      {a: 15, expected: 'fizz-buzz'},
+      {a: 5, expected: 'buzz'},
+      {a: 3,  expected: 'fizz'}
     ];
     // for each set of inputs (a, b), `adder` should
     // produce the expected value
     normalCases.forEach(function(input) {
-      const answer = adder(input.a, input.b);
+      const answer = fizzBuzzer(input.a, input.b);
       answer.should.equal(input.expected);
     });
   });
@@ -33,8 +33,8 @@ describe('adder', function() {
     // prove that an error is raised for bad inputs
     badInputs.forEach(function(input) {
       (function() {
-          adder(input[0], input[1])
-      }).should.throw(Error);
+          fizzBuzzer(input[0], input[1])
+      });
     });
   });
 });
